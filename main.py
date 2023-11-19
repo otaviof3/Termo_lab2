268f956b61828dcfc70947180a8deac53056085b
otaviof3 <126731189+otaviof3@users.noreply.github.com>
2023-11-19 14:47:10 -0300

    Update main.py
    New version.

--- main.py
+++ main.py
@@ -1,54 +1,24 @@
-import random
-def take_word():
-    number = random.randint(0,95)
-    file = open("palavras_termo.txt", "r")
-    aleatory_word = file.readlines()
-    word = aleatory_word[number]
-    return word
-    
-
-def user_input():
+import termo_game
+import os
+def clear_screen():
+    # Clear the terminal screen for various operating systems #
+    os.system('cls' if os.name == 'nt' else 'clear')
+def game_settings():
+    clear_screen()
+    # Termo, Dueto or Quarteto / Game choice #
+    print('TERMO\n')
+    print('Choose the game you wish to play:')
+    print('1 - Solo')
+    print('2 - Dueto')
+    print('3 - Quarteto')
+    print("Any other number to exit.")
     while True:
         try:
-            inputa = input("type >> ")
-            return inputa.upper()
+            gamemode = input("What game do you want to play? ")
+            return gamemode
         except ValueError:
-            print("oi - wot are you typ'ing")
-    
-
-class Compair:
-    def position(word, inputa):
-        #print("  ----  ", inputa, word)
-        resultado = "| "
-        for index in range(5):
-            if word[index] == inputa[index]:
-                resultado = resultado + f"\033[92m{inputa[index]}\033[0m | "
-            else:
-                resultado = resultado + f"{inputa[index]} | "
-        return resultado
-
-    def letter(word, inputa):
-        list_letter = []
-        resultado = "| "
-        for leatter in word:
-            for i in inputa:
-                if leatter in i:
-                    list_letter.append(leatter)
-        for letra in inputa:
-            if letra in list_letter:
-                resultado = resultado + f"\033[93m{letra}\033[0m | "
-            else:
-                resultado = resultado + f"{letra} | "
-        return resultado
-                    
-        
-
+            print('Please, type a valid option!')
 def main():
-    word = take_word()
-    inputa = user_input()
-    certo = Compair.position(word, inputa)
-    meio = Compair.letter(word, inputa)
-    print(certo)
-    print(meio)
-    #compair(word, inputa)
+    gamemode = game_settings()
+    termo_game.termo(gamemode)
 main()
