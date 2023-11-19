0e969ab8a8e124a3726db5a8386831bfd4464e1e
otaviof3 <126731189+otaviof3@users.noreply.github.com>
2023-11-19 00:50:36 -0300

    Update main.py
    Passo a passo

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+# Termo, Dueto ou Quarteto / Escolha do Jogo #
+# Defina palavra(s) escolhida(s) / Não podem ser as mesmas no Dueto ou Quarteto #
+# Escreva palavra #
+# Verifique se é válida / Está no .txt, são letras do alfabeto #
+# Compara com a(s) palavras(s) e define se é amarelo, verde ou preto #
+# 6, 7 ou 9 tentativas respectivamente #
+# Apenas conte tentativas caso a palavra seja válida #
+# Caso acerte (todas) a(s) palavra(s) pare o jogo e mande uma mensagem de sucesso #
+# Reiniciar o jogo? #
 import random
 def take_word():
     number = random.randint(0,95)
@@ -5,17 +14,18 @@ def take_word():
     aleatory_word = file.readlines()
     word = aleatory_word[number]
     return word
-    
-
 def user_input():
-    while True:
-        try:
-            inputa = input("type >> ")
-            return inputa.upper()
-        except ValueError:
-            print("oi - wot are you typ'ing")
-    
-
+    for i in range(5):
+        inputa = input("type >> ")
+        if len(inputa) != 5:
+            print('Error - digite uma palavra de 5 letras')
+        else:
+            try:
+                int(inputa)
+                raise InvalidInput ("type a word, not number")
+            except ValueError:
+                return inputa.upper()
+        return inputa
 class Compair:
     def position(word, inputa):
         #print("  ----  ", inputa, word)
@@ -26,7 +36,6 @@ class Compair:
             else:
                 resultado = resultado + f"{inputa[index]} | "
         return resultado
-
     def letter(word, inputa):
         list_letter = []
         resultado = "| "
@@ -40,9 +49,6 @@ class Compair:
             else:
                 resultado = resultado + f"{letra} | "
         return resultado
-                    
-        
-
 def main():
     word = take_word()
     inputa = user_input()
